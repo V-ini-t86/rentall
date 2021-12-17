af97609beab644477a2ace09211095b4ac0d4050
Ryan Emmans <ryanemmans@gmail.com>
2021-12-16 17:51:20 -0800

    Add login form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from "./components/login/Login";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ display: 'flex', height: '67vh', justifyContent: 'center', alignItems: 'center' }}>
       <Router>
         <Routes>
           <Route path="login" element={<Login />} />
